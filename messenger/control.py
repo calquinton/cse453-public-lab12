b146359c97d6a2efea518415a2cdd288317855e4
AaronBurnside <60364939+AaronBurnside@users.noreply.github.com>
2023-03-23 14:53:38 -0600

    implemented control levels

--- messenger/control.py
+++ messenger/control.py
@@ -10,4 +10,12 @@
 # you may need to put something here...
 class Control:
     def __init__(self, public, confidential, secret, top_secret):
+        if Value == 1:
+            Control_Level = "Confidental"
+        if Value == 2:
+            Control_Level = "Privledged"
+        if Value == 3:
+            Control_Level = "Secret"
+        else:
+            Control_Level = "Public"
         pass
